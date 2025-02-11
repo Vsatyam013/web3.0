6a2d8a9edb01e72bb6dad65cd8c6e3d6ee190775
vsatyam013 <vsatyam013@gmail.com>
2025-02-11 12:42:49 +0530

    first change

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import {Navbar} from "./components";
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+    </div>
+  </div>
+);
+
+export default App;
