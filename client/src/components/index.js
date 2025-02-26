f84a8795054269b48ee296783564a9aef5dadc0c
vsatyam013 <vsatyam013@gmail.com>
2025-02-26 18:39:37 +0530

    second commit

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Navbar } from "./navbar";
+export { default as Navbar } from "./Navbar";
 export { default as Welcome } from "./Welcome";
 export {default as Services} from "./Services";
 export {default as Transactions} from "./Transactions";
